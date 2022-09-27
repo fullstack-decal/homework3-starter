5c0661b5494f3fbc50ec5e789a64c41c5eb5e051
Dhruv Vaish <dvaish@berkeley.edu>
2022-09-27 00:34:55 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,68 @@
+// Q1: Function This
+// Testing variables, control flow, function composition
+function max(iterable, key) {
+    // Implement a function which finds the maximum in a list after applying    // function `key` to each element
+    // ex. max([3, 1, 2], (x) => x) = 3
+    // ex. max([-1, -4, 4, 6], (x) => 1 / x) = 4 
+    var maximum = _____;
+    for (var _____ in ______) {
+      var _____ = key(_____);
+      if (______ >= _____) {
+        ______ = ______;
+      }
+    }
+    return maximum;    
+}
+
+// Q2: Flip It and Reverse It
+// Testing anonymous (arrow) function and function composition
+const reverser = ___ => ___ // Replace this with your solution
+// Given a list of real numbers, write an anonymous function that we can pass as the key to `max` to return the smallest element
+// ex. max([1, -2, 3], reverser) = -2
+
+// Q3: What is This?
+// Testing knowledge of this, operators, and builtins
+const thiser = {
+    a: (Math.random() * 100),
+    b: (Math.random() * 100),
+    x: (Math.random() * 100),
+    y: (Math.random() * 100),
+    get distance() {
+        return Math.sqrt(
+            // return the Euclidean distance between the point (a, b) and (x, y)
+            // hint: Euclidean distance is given by sqrt((a - x)^2 + (b - y)^2) 
+            1 // replace this with your solution
+        )   
+    }  
+}
+
+// Q4: Objects Gworl
+// Testing Objects (JSON), Arrays, and Object Properties
+function changer(object, key, value) {
+    let prev = object[key]
+    if (Array.isArray(prev)) {
+        // what to do if value associated with key is an array
+    } else if ((typeof(prev) == 'object') && (prev != null)) {
+        // what to do if value is a JSON object
+    } else {
+        // what to do if neither
+    }
+    return null
+}
+
+// Q5: Promises Promises
+// Testing asynchronous, promises, 
+async function promise_me(truthy) {
+    const a = new Promise((resolve, reject) => {
+        // use this line to print the first value
+        // use this line to resolve the value that we will need later
+    })
+    console.log(2)
+    console.log(3)
+    console.log(4)
+    const five = ___ // how do we get the value that we saved earlier?
+    console.log(five)
+    return five
+}
+
+promise_me()
